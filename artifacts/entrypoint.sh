f3955773707cb37b4be5c67d5da102ffe4fc9310
Toon Leijtens <toon.leijtens@gmail.com>
2018-11-06 16:55:42 +0100

    removed leftover from old code.

--- artifacts/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-OPTIONS="--rpc --rpcport $RPCPORT --rpcaddr 0.0.0.0 --datadir /root/.ethereum --etherbase adminetherbase --verbosity 6"
-
-/usr/local/sbin/geth  --rinkeby --cache=1024 --rpccorsdomain="*" --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3 --identity $1 $OPTIONS
